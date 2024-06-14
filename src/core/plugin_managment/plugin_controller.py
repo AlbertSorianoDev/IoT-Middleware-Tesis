34774e6d32b9074ff7e83f57beb217114524490a
Alberto Soriano <albertosoriano.e@gmail.com>
2024-06-14 13:03:22 -0500

    refactor and fix: reorganizaded plugins per protocol type and fixed load_plugins to load them by searching inside folders

--- src/core/plugin_managment/plugin_controller.py
+++ src/core/plugin_managment/plugin_controller.py
@@ -39,43 +39,50 @@ class PluginController(metaclass=SingletonMeta):
         for actuator_subclass in self.actuator_factory.actuator_classes.values():
             self.plugin_interfaces.append(actuator_subclass.PLUGIN_INTERFACE)
 
-        for plugin_class in self.plugin_interfaces:
-            self.plugins_type_index[plugin_class.__name__] = []
-            self.plugin_interfaces_index[plugin_class.__name__] = plugin_class
+        for plugin_interface in self.plugin_interfaces:
+            self.plugins_type_index[plugin_interface.__name__] = []
+            self.plugin_interfaces_index[plugin_interface.__name__] = plugin_interface
 
     def load_plugins(self):
-        for filename in os.listdir(self.plugins_folder):
+        for root, __, files in os.walk(self.plugins_folder):
 
-            if filename.endswith(".py") and filename != "__init__.py":
-                module_name: str = filename[:-3]
-                file_path: str = os.path.join(self.plugins_folder, filename)
-                spec = importlib.util.spec_from_file_location(module_name, file_path)
-                module = importlib.util.module_from_spec(spec)
-                spec.loader.exec_module(module)
+            for filename in files:
 
-                for attribute_name in dir(module):
-                    attribute = getattr(module, attribute_name)
+                if filename.endswith(".py") and filename != "__init__.py":
+                    module_name = filename[:-3]
+                    file_path = os.path.join(root, filename)
+                    spec = importlib.util.spec_from_file_location(
+                        module_name, file_path
+                    )
+                    module = importlib.util.module_from_spec(spec)
+                    spec.loader.exec_module(module)
 
-                    if not isinstance(attribute, type):
-                        continue
+                    for attribute_name in dir(module):
+                        attribute = getattr(module, attribute_name)
 
-                    for interface_class in self.plugin_interfaces:
-                        if (
-                            issubclass(attribute, interface_class)
-                            and attribute is not interface_class
-                        ):
-                            new_plugin = Plugin(
-                                cls_name=attribute.__name__,
-                                module_name=module_name,
-                                cls=attribute,
-                            )
+                        if not isinstance(attribute, type):
+                            continue
 
-                            self.plugins_type_index[interface_class.__name__].append(
-                                new_plugin
-                            )
-                            self.plugins_class_index[attribute.__name__] = new_plugin
+                        for interface_class in self.plugin_interfaces:
+                            if (
+                                issubclass(attribute, interface_class)
+                                and attribute is not interface_class
+                            ):
+                                new_plugin = Plugin(
+                                    cls_name=attribute.__name__,
+                                    module_name=module_name,
+                                    cls=attribute,
+                                )
 
-                            break
+                                self.plugins_type_index[
+                                    interface_class.__name__
+                                ].append(new_plugin)
+
+                                self.plugins_class_index[attribute.__name__] = (
+                                    new_plugin
+                                )
+
+                                break
 
     def get_plugin_by_class_name(self, plugin_class_name: str) -> Plugin:
         return self.plugins_class_index.get(plugin_class_name)
